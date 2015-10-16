f7d510a9147f3ca990593d155ed73feca34c1596
mSenyor <moran.senyor@gmail.com>
2015-10-16 15:47:15 +0300

    decimal controls fully functional. decimal display reacts to decimal controls only./genymotion binary switches respond to decimal controls.

--- main.py
+++ main.py
@@ -19,12 +19,14 @@ bin_address='0b0'
 str_dec='000'
 str_bnr='000000000'
 
+
+### debug helper START
 def stat():
     print "key variables\n\tdec_address: %d\n\tbin_address: %s\n\tstr_dec: %s\n\tstr_bin: %s" % (dec_address, bin_address, str_dec, str_bnr)
     sleep(1)
 
 stat()
-
+### debug helper END
 # to turn integers to binary use python's built in bin(int) function
 
 # to turn str_dec to integer use python's built in int(string) function
@@ -99,37 +101,51 @@ def all_str_bnr():
     bin_address=bin(dec_address)
     str_dec=str_int(dec_address)
 
-### dec_address add subtract methods ###
+### dec_address add, subtract, and set methods ###
 # add
 def dec_add(num):
     global dec_address
     dec_address+=num
+    all_dec()
 # subtract
 def dec_sub(num):
     global dec_address
     dec_address-=num
+    all_dec()
+# set
+def dec_set(val):
+    global dec_address
+    if val >= 0 and val <= 511:
+        dec_address = val
+        all_dec()
+    else:
+        print "dec_set() function was passed an invalid value"
 
 ### str_dec add subtract methods ###
 # add
 def str_dec_add(num):
     global str_dec
     str_dec=str_int(int(str_dec)+num)
+    all_str_dec()
 
 # subtract
 def str_dec_sub(num):
     global str_dec
     str_dec=str_int(int(str_dec)-num)
+    all_str_dec()
 
 ### str_bnr add subtract methods ###
 # add
 def str_bnr_add(num):
     global str_bnr
     str_bnr=str_bin(bin(int_bnr(str_bnr)+num))
+    all_str_bnr()
 
 # subtract
 def str_bnr_sub(num):
     global str_bnr
     str_bnr=str_bin(bin(int_bnr(str_bnr)-num))
+    all_str_bnr()
 
 
 ### str_bnr individual digit 0/1 toggle ###
@@ -157,6 +173,13 @@ def togg_str_bnr(index):
         after=str_bnr[(index*-1)+1:]
     digit=zero_one(digit)
     str_bnr=before+digit+after
+
+
+dec_add(1)
+stat()
+
+
+################### GUI starts here #############################
 
 ### A
 # A1 the object containing the main view of the program
@@ -195,24 +218,210 @@ class BinaryView(BoxLayout):
     pass
 
 ### ### ### C
-# C4
-class SingleSwitch(BoxLayout):
+# C4-C12 these are 9 SingleDip objects for each of the 9 dipswitch switches
+# each one containing DipVal, BinSwitch, and DipNum objects in this order.
+# this totals to 27 objects marked D4-D30 in the dipswitch.kv file
+class SingleDip(BoxLayout):
     pass
+
+### ### ### ### D
+# D4, D7, D10, D13, D16, D19, D22, D25, and D28, a total of 9 DipVal objects.
+# each marks the binary digit value of the switch blow it.
+class DipVal(Label):
+    pass
+# D5, D8, D11, D14, D17, D20, D23, D26, and D29, a total of 9 BinSwitch objects.
+class BinSwitch(Switch):
+    pass
+# D6, D9, D12, D15, D18, D21, D24, D27, and D30, a total of 9 DipNum objects.
+class DipNum(Label):
+    pass
+
+
 
 
 
 ####### main #######
 class DipSwitch(App):
+    # address holders
+    decimal=StringProperty(str_dec)
+    binary=StringProperty(str_bnr)
+
+    # called on start
     def build(self):
+        self.str_bnr_dip()
         return MainView()
 
-    def out_str_dec(self):
-        if str_dec == '000':
-            return '512'
-        elif dec_address < 512 and dec_address > 0:
-            return str_dec
+    # updates the address holders
+    def refresh(self):
+        if str_dec != '000':
+            self.decimal=str_dec
         else:
-            return 'error'
+            self.decimal='512'
+        self.binary=str_bnr
+        self.str_bnr_dip()
+
+    #def out_str_dec(self):
+        #if str_dec == '000':
+            #return '512'
+        #elif dec_address < 512 and dec_address > 0:
+            #return str_dec
+        #else:
+            #return 'error'
+
+######## different button functions for the DecimalView object
+
+    # raises the whole address by one
+    def all_up(self):
+        if dec_address < 511:
+            dec_add(1)
+        elif dec_address == 511:
+            dec_set(0)
+        self.refresh()
+
+    # lowers the whole address by one
+    def all_down(self):
+        if dec_address > 0:
+            dec_sub(1)
+        elif dec_address == 0:
+            dec_set(511)
+        self.refresh()
+
+    ### specific digit changes
+    
+    # raises the hundreds digit by one
+    def hund_up(self):
+        if dec_address < 412 and dec_address != 0:
+            dec_add(100)
+        elif dec_address == 412:
+            dec_set(0)
+        elif dec_address == 500:
+            dec_set(1)
+        elif dec_address > 500:
+            dec_sub(500)
+        elif dec_address == 0:
+            dec_set(1)
+        else: # what do i do with this?
+            dec_set(511)
+        self.refresh()
+
+    # raise the tens digit by one
+    def ten_up(self):
+        tens=int(str_dec[1])
+        if dec_address == 90:
+            dec_set(1)
+        elif dec_address < 500 and dec_address != 0:
+            if tens != 9:
+                dec_add(10)
+            else:
+                dec_sub(90)
+        elif dec_address >= 500 and dec_address < 502:
+            if tens == 0:
+                dec_add(10)
+            if tens == 1:
+                dec_sub(10)
+        elif dec_address >= 502 and dec_address < 510:
+            dec_set(0)
+        elif dec_address == 510 or dec_address == 511:
+            dec_sub(10)
+        else:
+            dec_set(502)
+        self.refresh()
+
+    # raise the ones digit by one
+    def one_up(self):
+        ones=int(str_dec[2])
+        if dec_address <= 510 and dec_address > 9:
+            if ones != 9:
+                dec_add(1)
+            else:
+                dec_sub(9)
+        elif dec_address == 511:
+            dec_set(0)
+        elif dec_address <= 8 and dec_address != 0:
+            dec_add(1)
+        elif dec_address == 9:
+            dec_set(1)
+        else:
+            dec_set(510)
+        self.refresh()
+
+    # lower the hundreds digit by one
+    def hund_down(self):
+        if dec_address > 100:
+            dec_sub(100)
+        elif dec_address == 100:
+            dec_set(1)
+        elif dec_address < 100 and dec_address >= 12:
+            dec_set(0)
+        elif dec_address < 12 and dec_address != 0:
+            dec_add(500)
+        else:
+            dec_set(412)
+        self.refresh()
+
+    # lower the tens digit by one
+    def ten_down(self):
+        tens=int(str_dec[1])
+        if dec_address == 0:
+            dec_set(502)
+        elif dec_address >= 502 and dec_address < 510:
+            dec_set(0)
+        elif dec_address == 500 or dec_address == 501:
+            dec_add(10)
+        elif dec_address == 511 or dec_address == 510:
+            dec_sub(10)
+        elif dec_address < 500 and dec_address != 0 and dec_address != 10:
+            if tens != 0:
+                dec_sub(10)
+            else:
+                dec_add(90)
+        elif dec_address == 10:
+            dec_set(90)
+
+        self.refresh()
+
+    # lower the ones digit by one
+    def one_down(self):
+        ones=int(str_dec[2])
+        if dec_address == 0:
+            dec_set(511)
+        elif dec_address == 510:
+            dec_set(0)
+        elif dec_address > 1 and dec_address != 510:
+            if ones != 0:
+                dec_sub(1)
+            else:
+                dec_add(9)
+        elif dec_address == 1:
+            dec_set(9)
+        self.refresh()
+
+
+############### BinaryView switch related functions
+
+# a list of 9 True/False values to represent the different active states of the switches
+            
+
+    state9=BooleanProperty(False)
+    state8=BooleanProperty(False)
+    state7=BooleanProperty(False)
+    state6=BooleanProperty(False)
+    state5=BooleanProperty(False)
+    state4=BooleanProperty(False)
+    state3=BooleanProperty(False)
+    state2=BooleanProperty(False)
+    state1=BooleanProperty(False)
+
+    dips=[state9, state8, state7, state6, state5, state4, state3, state2, state1]
+    def str_bnr_dip(self):
+        for i in range(0,9):
+            if str_bnr[i] == '0':
+                self.dips[i].set(self,False)
+            else:
+                self.dips[i].set(self,True)
+
+
+        
 
 if __name__ == "__main__":
     DipSwitch().run()
@@ -259,4 +468,4 @@ if __name__ == "__main__":
 #all_str_bnr()
 #stat()
 #
-print "\n\n\n\n\n\n\nALL DONE!"
+print "\nALL DONE!"
